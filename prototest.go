9d4a76056e1f5c131a771b2e171da953a3a79fc4
yvasv98 <yashuk.vashisht@anz.com>
2022-12-01 08:46:10 +1100

    adding in comments

--- prototest.go
+++ prototest.go
@@ -7,6 +7,11 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// ProtoTest wraps proto.Equal wraps proto.Equal with a detailed diff analysis.
+//
+// proto.Equal is used to determine if the two proto objects are equal and return a bool if so.
+// If not, it makes use of assert.fail func to provided detailed analysis of the diff between
+// the expected and the actual.
 func ProtoTest(t assert.TestingT, expected proto.Message, actual proto.Message) bool {
 	if !proto.Equal(expected, actual) {
 		return assert.Fail(t, fmt.Sprintf("Not equal: \n"+
