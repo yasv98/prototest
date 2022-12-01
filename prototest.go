7d3e046ff6d7731c32740a2bf8c4ec25f774580d
yvasv98 <yashuk.vashisht@anz.com>
2022-12-01 11:17:05 +1100

    defining the test in func

--- prototest.go
+++ prototest.go
@@ -12,11 +12,16 @@ import (
 // proto.Equal is used to determine if the two proto objects are equal and return a bool if so.
 // If not, it makes use of assert.fail func to provided detailed analysis of the diff between
 // the expected and the actual.
-func ProtoTest(t assert.TestingT, expected proto.Message, actual proto.Message) bool {
+func ProtoTest(t TestingT, expected proto.Message, actual proto.Message) bool {
 	if !proto.Equal(expected, actual) {
 		return assert.Fail(t, fmt.Sprintf("Not equal: \n"+
 			"expected: %s\n"+
 			"actual  : %s", actual, expected))
 	}
 	return true
+}
+
+// TestingT is an interface wrapper around *testing.T
+type TestingT interface {
+	Errorf(format string, args ...interface{})
 }
