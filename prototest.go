e4cd34434ec30cd7f27c14803fe25fea565391cf
yasv98 <112038876+yasv98@users.noreply.github.com>
2023-05-30 16:23:36 +1000

    Update prototest.go

--- prototest.go
+++ prototest.go
@@ -3,7 +3,9 @@ package prototest
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/testing/protocmp"
 )
 
 // TestingT is an interface wrapper around *testing.T.
@@ -11,16 +13,20 @@ type TestingT interface {
 	Errorf(format string, args ...interface{})
 }
 
-// Equal wraps proto.Equal with a detailed diff analysis.
+// Equal implements cmp.Equal with nice test output for debugging.
 //
-// proto.Equal is used to determine if the two proto objects are equal and return a bool if so.
-// If not, it makes use of the func Fail to provide a detailed analysis of the diff between
-// the expected and the actual output.
-func Equal(t TestingT, expected proto.Message, actual proto.Message) bool {
-	if !proto.Equal(expected, actual) {
-		return fail(t, fmt.Sprintf("Not equal: \n"+
-			"expected: %s\n"+
-			"actual  : %s", expected, actual))
+// cmp.Diff is used to determine if the two proto objects are equal and returns a string
+// representation of the diff if they are not. If Diff fails, the output is logged to test
+// output, with a location of where the comparison failed.
+//
+// Options SHOULD be provided by the protocmp package. The protocmp.Transform() option does
+// NOT need to be provided, it is supplied for you. EG:
+//
+//	prototest.Equal(t, message1, message2, protocmp.IgnoreFields(message2, "my_field"))
+func Equal(t TestingT, expected proto.Message, actual proto.Message, opts ...cmp.Option) bool {
+	opts = append(opts, protocmp.Transform())
+	if diff := cmp.Diff(expected, actual, opts...); diff != "" {
+		return fail(t, fmt.Sprintf("Not equal: \n"+diff))
 	}
 	return true
 }
